c0c242169a2e0c424e16d1d95c316cb828e14083
jizrix <jizrix@gmail.com>
2023-10-25 17:26:49 +0700

    fix https bug

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const enforce = require('express-enforces-ssl');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const UserModel = require('./models/User');
 const multer = require('multer');
-
 const { v4: uuidv4 } = require('uuid');
 const bcrypt = require('bcrypt');
 const ActivityModel = require('./models/Activity');
@@ -29,7 +27,6 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(express.static('public'));
 
 const uri =
